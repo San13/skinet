0411bfa11a84fb6869cb29b34f05789c9da6c116
kaung san <kaungs542@gmail.com>
2020-05-23 22:12:56 +0800

    Frontend Filtering, Sorting, Paging, Searching UI. close *8

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagintation';
+import { IProduct } from './shared/models/product';
+import { IPagination } from './shared/models/pagintation';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +10,8 @@ import { IPagination } from './models/pagintation';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: IProduct[];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-     this.http.get('https://localhost:5001/api/products')
-      .subscribe((response: IPagination) => {
-              this.products = response.data;
-     }, error => {
-          console.log(error);
-     });
+  
+  ngOnInit() {
   }
-} 
+ 
+}
