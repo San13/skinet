f6c9dfc9847e4b019bbeb02c8e21238024accc5a
kaung san <kaungs542@gmail.com>
2020-05-26 00:06:22 +0800

    Basket Ul, Add Item to Basket, Increment, Decrement, Summary close #13

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IProduct } from './shared/models/product';
 import { IPagination } from './shared/models/pagintation';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,19 @@ import { IPagination } from './shared/models/pagintation';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  
+
+  constructor(private basketService: BasketService) {
+ 
+  }
   ngOnInit() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+         console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
  
 }
