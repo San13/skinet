eac16055f13fbcfc7bc0297e8eb9c2421a492b0c
kaung san <kaungs542@gmail.com>
2020-05-26 17:10:18 +0800

    Add Validation in Dto classes and Update Swagger to have authorise feature close #15

--- API/Helpers/MappingProfile.cs
+++ API/Helpers/MappingProfile.cs
@@ -13,6 +13,8 @@ namespace API.Helpers
             .ForMember( d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
             .ForMember( d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
              CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
+             CreateMap<CustomerBasketDto, CustomerBasket>();
+             CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
